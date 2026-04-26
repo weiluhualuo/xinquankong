79cb40fcf358b144b474ce4b188859d852e7ccd7
洛子于 <1753242279@qq.com>
2026-04-26 23:42:31 +0800

    Remove remaining UTF-8 BOM files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-﻿import type { Metadata } from "next";
+import type { Metadata } from "next";
 import { SiteHeader } from "../components/site-header";
 import "./globals.css";
 
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,4 +1,4 @@
-﻿"use client";
+"use client";
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
